377cfe49ad00d4c40f698a47dc359df56899de2a
Cameron Lai <cameronjml2@gmail.com>
2015-02-20 14:02:37 +0800

    Fix urls and views bug in showing index.html

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-from examTimeTableGenerator import views
+from EXT_GEN import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
